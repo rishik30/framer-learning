e714306d70d4da6a67e37d7f2589e4531d579e44
rishik30 <rishabhk3003@gmail.com>
2024-09-19 22:33:20 +0530

    feat: added google fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import {Archivo_Black, Oxygen} from 'next/font/google'
+
+export const archivoBlack = Archivo_Black({weight: '400', subsets: ['latin']})
+export const oxygenFont = Oxygen({weight: ['300', '400', '700'], subsets: ['latin']})
